9bc25d5439302f1879048264a475a6704f5b5d14
Rémi Saurel <r4.saurel@gmail.com>
2015-11-06 17:44:23 +0100

    Squelette client

---
+++ host/simon_gui/MainWindow.cpp
@@ -0,0 +1,17 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include <iostream>
+
+using namespace std::literals;
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), _ui(std::make_unique<Ui::MainWindow>()) {
+    _ui->setupUi(this);
+    this->connect(_ui->myButton, SIGNAL(clicked()), this, SLOT(onClickClick()));
+}
+
+MainWindow::~MainWindow() {}
+
+
+void MainWindow::onClickClick() {
+    std::cout << "Sending \"" << _ui->text->toPlainText().toUtf8().constData() << "\" to the network!" << std::endl;
+}
--- host/simon_gui/Mainwindow.cpp
+++
@@ -1,8 +0,0 @@
-#include "Mainwindow.h"
-#include "ui_mainwindow.h"
-
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), _ui(std::make_unique<Ui::MainWindow>()) {
-    _ui->setupUi(this);
-}
-
-MainWindow::~MainWindow() {}
