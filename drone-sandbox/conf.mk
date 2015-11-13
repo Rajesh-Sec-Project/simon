0ca0976781c678989458ac88137fd04e7c6abf53
Rémi Saurel <r4.saurel@gmail.com>
2015-11-13 16:07:48 +0100

    drone-sandbox

---
+++ drone-sandbox/conf.mk
@@ -0,0 +1,42 @@
+#################################
+### Directories configuration ###
+#################################
+
+SRC_DIR=src
+INC_DIR=inc
+TST_DIR=tests
+TMP_DIR=build
+BIN_DIR=bin
+DBG_DIR=debug
+LIB_DIR=../libs.d
+
+QEMU_ARM=qemu-arm
+QEMU_SYSROOT=$(HOME)/x-tools/arm-cortex_a8-linux-gnueabi/arm-cortex_a8-linux-gnueabi
+
+#####################
+### Project names ###
+#####################
+
+PROJECT=drone-sandbox
+PRODUCT=drone-sandbox
+
+#############
+### Flags ###
+#############
+
+CX_FLAGS = -std=gnu++14 -static-libstdc++
+
+release: CX_FLAGS += -O3
+debug:   CX_FLAGS += -O0 -g
+
+LD_FLAGS = -lm -static-libstdc++ -lcomm -lconf -lpthread
+
+CROSS = 1
+DEPENDS = libcomm libconf
+
+#############
+### Tools ###
+#############
+
+CX=arm-cortex_a8-linux-gnueabi-g++
+LD=$(CX)
