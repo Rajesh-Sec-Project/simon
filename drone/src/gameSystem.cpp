744a5f321fa3006425f29e24dbed29cfad7f8e8b
Rémi Saurel <r4.saurel@gmail.com>
2015-11-16 18:35:24 +0100

    Better logging of user actions in the host's application.

--- drone/src/gameSystem.cpp
+++ drone/src/gameSystem.cpp
@@ -14,11 +14,81 @@ using namespace lcontrol;
 void GamePadSubscriber::notify(lcomm::Endpoint& ep, lcomm::PacketBase const& packet) {
     GamepadPacket* ctrl = packet.downcast<GamepadPacket>();
     if(ctrl) {
-        std::cout << "Received: " << ctrl->keys() << std::endl;
         if(ctrl->keys() & GamepadPacket::Land) {
             Control::land();
         } else if(ctrl->keys() & GamepadPacket::TakeOff) {
             Control::takeoff();
+        }
+        else if(ctrl->keys() & GamepadPacket::Up) {
+            std::cout << "Detected a movement to the top:\n"
+                                 "        / \\\n"
+                                 "      /     \\\n"
+                                 "    /__     __\\\n"
+                                 "       |   |\n"
+                                 "       |   |\n"
+                                 "       |   |\n"
+                                 "       |   |\n"
+                                 "       |___|"
+            << std::endl;
+        }
+        else if(ctrl->keys() & GamepadPacket::Down) {
+            std::cout << "Detected a movement to the bottom:\n"
+                                 "        ___\n"
+                                 "       |   |\n"
+                                 "       |   |\n"
+                                 "       |   |\n"
+                                 "       |   |\n"
+                                 "     __|   |__\n"
+                                 "    \\         /\n"
+                                 "      \\     /\n"
+                                 "        \\ /"
+            << std::endl;
+        }
+        else if(ctrl->keys() & GamepadPacket::Left) {
+            std::cout << "Detected a movement to the left:\n"
+                                 "  /|\n"
+                                 " / |_____________\n"
+                                 "/                |\n"
+                                 "\\   _____________|\n"
+                                 " \\ |\n"
+                                 "  \\|"
+            << std::endl;
+        }
+        else if(ctrl->keys() & GamepadPacket::Right) {
+            std::cout << "Detected a movement to the right:\n"
+                                 "              |\\\n"
+                                 " _____________| \\\n"
+                                 "|                \\\n"
+                                 "|_____________   /\n"
+                                 "              | /\n"
+                                 "              |/"
+            << std::endl;
+        }
+        else if(ctrl->keys() & GamepadPacket::Stop) {
+            std::cout << "Stop requested:\n"
+                                 "         ________________\n"
+                                 "       /.--------------.\\\n"
+                                 "      //                \\\\\n"
+                                 "     //                  \\\\\n"
+                                 "    || .-..----. .-. .--. ||\n"
+                                 "    ||( ( '-..-'|.-.||.-.|||\n"
+                                 "    || \\ \\  ||  || ||||_||||\n"
+                                 "    ||._) ) ||  \\'-'/||-' ||\n"
+                                 "     \\\\'-'  `'   `-' `'  //\n"
+                                 "      \\\\                //\n"
+                                 "       \\\\______________//\n"
+                                 "        '--------------'\n"
+                                 "              |_|_\n"
+                                 "       ____ _/ _)_)\n"
+                                 "           '  | (_)\n"
+                                 "        .--'\"\\| ()\n"
+                                 "              | |\n"
+                                 "              | |\n"
+                                 "              |_|"
+                    << std::endl;
+
+            Control::land();
+            std::exit(0);
         }
     }
 }
