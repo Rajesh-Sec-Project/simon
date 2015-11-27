aeda10d5023ac20baf7a14e8e7d853caaebd483d
Rémi Saurel <r4.saurel@gmail.com>
2015-11-27 21:42:44 +0100

    Main

--- host/simon_gui/main.cpp
+++ host/simon_gui/main.cpp
@@ -15,12 +15,12 @@ int main(int argc, char* argv[]) {
     lcomm::PacketManager::registerPacketClass<lcomm::InfoPacket>();
 
     qDebug() << "Waiting for connection...";
-    //while(!CommManager::self().opened())
-    //    ;
+    while(!CommManager::self().opened())
+        ;
     qDebug() << "Connected !";
 
     MainWindow w;
     w.show();
 
-    std::exit(a.exec());
+    a.exec();
 }
