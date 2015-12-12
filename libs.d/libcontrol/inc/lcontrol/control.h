00bc79ffb1beff2c536f53d454def6d303c7b00c
Rémi Saurel <r4.saurel@gmail.com>
2015-12-12 12:54:55 +0100

    libcontrol is now thread-safe

--- libs.d/libcontrol/inc/lcontrol/control.h
+++ libs.d/libcontrol/inc/lcontrol/control.h
@@ -1,6 +1,7 @@
 #include "lcomm/lcomm.h"
 #include <memory>
 #include <atomic>
+#include <mutex>
 
 namespace lcontrol {
     class Control {
@@ -25,41 +26,43 @@ namespace lcontrol {
     private:
         static std::string float_to_string(float i);
 
-        static void sendNavData(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void watchdog(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void emergencyMode(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void horizontalPlan(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void magnetometer(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void takeoff(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void land(std::uint32_t seqNum, lcomm::ClientSocket& s);
+        static void sendNavData(std::uint32_t seqNum);
+        static void watchdog(std::uint32_t seqNum);
+        static void emergencyMode(std::uint32_t seqNum);
+        static void horizontalPlan(std::uint32_t seqNum);
+        static void magnetometer(std::uint32_t seqNum);
+        static void takeoff(std::uint32_t seqNum);
+        static void land(std::uint32_t seqNum);
         static void movement(std::uint32_t seqNum,
                              int flag,
                              float frontBackTilt,
                              float leftRightTilt,
                              float verticalSpeed,
-                             float angularSpeed,
-                             lcomm::ClientSocket& s);
-        static void ackControl(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void getCfgControl(std::uint32_t seqNum, lcomm::ClientSocket& s);
+                             float angularSpeed);
+        static void ackControl(std::uint32_t seqNum);
+        static void getCfgControl(std::uint32_t seqNum);
 
-        static void pmode(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void misc(std::uint32_t seqNum, lcomm::ClientSocket& s);
+        static void pmode(std::uint32_t seqNum);
+        static void misc(std::uint32_t seqNum);
 
-        static void sessionid(std::uint32_t seqNum, std::string const& id, lcomm::ClientSocket& s);
-        static void appid(std::uint32_t seqNum, std::string const& id, lcomm::ClientSocket& s);
-        static void appdesc(std::uint32_t seqNum, std::string const& desc, lcomm::ClientSocket& s);
-        static void sessiondesc(std::uint32_t seqNum, std::string const& desc, lcomm::ClientSocket& s);
+        static void sessionid(std::uint32_t seqNum, std::string const& id);
+        static void appid(std::uint32_t seqNum, std::string const& id);
+        static void appdesc(std::uint32_t seqNum, std::string const& desc);
+        static void sessiondesc(std::uint32_t seqNum, std::string const& desc);
 
-        static void configids(std::uint32_t seqNum, lcomm::ClientSocket& s);
-        static void config(std::uint32_t seqNum, std::string const& key, std::string const& value, lcomm::ClientSocket& s);
-
-        static std::atomic<std::uint32_t> m_seqNum;
-        static std::unique_ptr<lcomm::ClientSocket> m_sock;
+        static void configids(std::uint32_t seqNums);
+        static void config(std::uint32_t seqNum, std::string const& key, std::string const& value);
 
         static void M_trace(std::string const& msg);
         static void M_traceFrame(std::string const& msg);
 
+        static void write(std::string const &data);
+
     private:
+        static std::atomic<std::uint32_t> m_seqNum;
+        static std::unique_ptr<lcomm::ClientSocket> m_sock;
+
         static std::string m_session_id, m_app_id;
+        static std::mutex m_mutex;
     };
 }
--- libs.d/libcontrol/src/control.cpp
+++ libs.d/libcontrol/src/control.cpp
@@ -57,216 +57,216 @@ namespace lcontrol {
     }
 
     void Control::watchdog() {
-        watchdog(m_seqNum.fetch_add(1), *m_sock);
+        watchdog(m_seqNum.fetch_add(1));
     }
 
     void Control::enableStabilization() {
         for(int i = 0; i < NUM_ITER; ++i) {
-            Control::horizontalPlan(m_seqNum.fetch_add(1), *m_sock);
+            Control::horizontalPlan(m_seqNum.fetch_add(1));
             std::this_thread::sleep_for(SLEEP_DELAY);
         }
     }
 
     void Control::takeoff() {
         for(int i = 0; i < NUM_ITER; ++i) {
-            Control::takeoff(m_seqNum.fetch_add(1), *m_sock);
+            Control::takeoff(m_seqNum.fetch_add(1));
             std::this_thread::sleep_for(SLEEP_DELAY);
         }
     }
 
     void Control::land() {
         for(int i = 0; i < NUM_ITER; ++i) {
-            Control::land(m_seqNum.fetch_add(1), *m_sock);
+            Control::land(m_seqNum.fetch_add(1));
             std::this_thread::sleep_for(SLEEP_DELAY);
         }
     }
 
     void Control::config(std::string const& key, std::string const& value) {
-        Control::configids(m_seqNum.fetch_add(1), *m_sock);
-        Control::config(m_seqNum.fetch_add(1), key, value, *m_sock);
+        Control::configids(m_seqNum.fetch_add(1));
+        Control::config(m_seqNum.fetch_add(1), key, value);
     }
 
     void Control::ackControl() {
-        Control::ackControl(m_seqNum.fetch_add(1), *m_sock);
+        Control::ackControl(m_seqNum.fetch_add(1));
     }
 
     void Control::getCfgControl() {
-        Control::getCfgControl(m_seqNum.fetch_add(1), *m_sock);
+        Control::getCfgControl(m_seqNum.fetch_add(1));
     }
 
     void Control::clearSessionId() {
-        Control::configids(m_seqNum.fetch_add(1), *m_sock);
-        Control::sessionid(m_seqNum.fetch_add(1), "-all", *m_sock);
+        Control::configids(m_seqNum.fetch_add(1));
+        Control::sessionid(m_seqNum.fetch_add(1), "-all");
     }
 
     void Control::setSessionId() {
-        Control::configids(m_seqNum.fetch_add(1), *m_sock);
-        Control::sessionid(m_seqNum.fetch_add(1), m_session_id, *m_sock);
+        Control::configids(m_seqNum.fetch_add(1));
+        Control::sessionid(m_seqNum.fetch_add(1), m_session_id);
     }
 
     void Control::setAppId() {
-        Control::configids(m_seqNum.fetch_add(1), *m_sock);
-        Control::appid(m_seqNum.fetch_add(1), m_app_id, *m_sock);
+        Control::configids(m_seqNum.fetch_add(1));
+        Control::appid(m_seqNum.fetch_add(1), m_app_id);
     }
 
     void Control::setAppDesc() {
-        Control::configids(m_seqNum.fetch_add(1), *m_sock);
-        Control::appdesc(m_seqNum.fetch_add(1), "Simon application", *m_sock);
+        Control::configids(m_seqNum.fetch_add(1));
+        Control::appdesc(m_seqNum.fetch_add(1), "Simon application");
     }
 
     void Control::setSessionDesc() {
-        Control::configids(m_seqNum.fetch_add(1), *m_sock);
-        Control::sessiondesc(m_seqNum.fetch_add(1), "Simon session", *m_sock);
+        Control::configids(m_seqNum.fetch_add(1));
+        Control::sessiondesc(m_seqNum.fetch_add(1), "Simon session");
     }
 
     void Control::strangeInit() {
-        Control::pmode(m_seqNum.fetch_add(1), *m_sock);
-        Control::misc(m_seqNum.fetch_add(1), *m_sock);
+        Control::pmode(m_seqNum.fetch_add(1));
+        Control::misc(m_seqNum.fetch_add(1));
     }
     void Control::movement(int flag, float frontBackTilt, float leftRightTilt, float verticalSpeed, float angularSpeed) {
-        movement(m_seqNum.fetch_add(1), flag, frontBackTilt, leftRightTilt, verticalSpeed, angularSpeed, *m_sock);
+        movement(m_seqNum.fetch_add(1), flag, frontBackTilt, leftRightTilt, verticalSpeed, angularSpeed);
     }
     // Ask the drone to send the navdata :
     // AT*CONFIG="seqNum",\"general:navdata_demo\",\"TRUE\"\r
-    void Control::sendNavData(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::sendNavData(std::uint32_t seqNum) {
         std::string data("AT*CONFIG=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + ",\"general:navdata_demo\",\"TRUE\"\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Reset the communication watchdog :
     // AT*COMWDG="seqNum"\r
-    void Control::watchdog(std::uint32_t, ClientSocket& s) {
+    void Control::watchdog(std::uint32_t) {
         std::string data("AT*COMWDG=1\r");
         // M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Emergency mode on the drone :
     // AT*REF="seqNum",290717952\r
-    void Control::emergencyMode(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::emergencyMode(std::uint32_t seqNum) {
         std::string data("AT*REF=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + ",290717952\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Calibrate the horizontal plan :
     // AT*FTRIM="seqNum"\r
-    void Control::horizontalPlan(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::horizontalPlan(std::uint32_t seqNum) {
         std::string data("AT*FTRIM=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + "\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Calibrate the magnetometer :
     // AT*CALIB="seqNum",0\r
-    void Control::magnetometer(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::magnetometer(std::uint32_t seqNum) {
         std::string data("AT*CALIB=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + ",0\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Take off :
     // AT*REF="seqNum",290718208\r
-    void Control::takeoff(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::takeoff(std::uint32_t seqNum) {
         std::string data("AT*REF=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + ",290718208\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Land :
     // AT*REF="seqNum",290717696\r
-    void Control::land(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::land(std::uint32_t seqNum) {
         std::string data("AT*REF=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + ",290717696\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Send a movement to the drone :
     // AT*PCMD="seqNum","flag","leftRightTilt","frontBackTilt","verticalSpeed","angularSpeed",\r
-    void Control::movement(std::uint32_t seqNum, int flag, float frontBackTilt, float leftRightTilt, float verticalSpeed, float angularSpeed, ClientSocket& s) {
+    void Control::movement(std::uint32_t seqNum, int flag, float frontBackTilt, float leftRightTilt, float verticalSpeed, float angularSpeed) {
         std::string data("AT*PCMD=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + "," + std::to_string(flag) + "," + Control::float_to_string(leftRightTilt) + "," +
                 Control::float_to_string(frontBackTilt) + "," + Control::float_to_string(verticalSpeed) + "," +
                 Control::float_to_string(angularSpeed) + "\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Send a control command ack, this resets the command_ack bit
     //   in the drone's state bitfield
-    void Control::ackControl(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::ackControl(std::uint32_t seqNum) {
         std::string data("AT*CTRL=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + ",5,0\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     // Get the drone's configuration dumped to the control port
     //   (TCP 5559)
-    void Control::getCfgControl(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::getCfgControl(std::uint32_t seqNum) {
         std::string data("AT*CTRL=");
         std::string seqStr = std::to_string(seqNum);
         data += seqStr + ",4,0\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
-    void Control::pmode(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::pmode(std::uint32_t seqNum) {
         std::string data("AT*PMODE=");
         data += std::to_string(seqNum) + ",2\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
-    void Control::misc(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::misc(std::uint32_t seqNum) {
         std::string data("AT*MISC=");
         data += std::to_string(seqNum) + ",2,20,2000,3000\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
-    void Control::sessionid(std::uint32_t seqNum, std::string const& id, lcomm::ClientSocket& s) {
-        config(seqNum, "custom:session_id", id, s);
+    void Control::sessionid(std::uint32_t seqNum, std::string const& id) {
+        config(seqNum, "custom:session_id", id);
     }
 
-    void Control::appid(std::uint32_t seqNum, std::string const& id, lcomm::ClientSocket& s) {
-        config(seqNum, "custom:application_id", id, s);
+    void Control::appid(std::uint32_t seqNum, std::string const& id) {
+        config(seqNum, "custom:application_id", id);
     }
 
-    void Control::appdesc(std::uint32_t seqNum, std::string const& desc, lcomm::ClientSocket& s) {
-        config(seqNum, "custom:application_desc", desc, s);
+    void Control::appdesc(std::uint32_t seqNum, std::string const& desc) {
+        config(seqNum, "custom:application_desc", desc);
     }
 
-    void Control::sessiondesc(std::uint32_t seqNum, std::string const& desc, lcomm::ClientSocket& s) {
-        config(seqNum, "custom:session_desc", desc, s);
+    void Control::sessiondesc(std::uint32_t seqNum, std::string const& desc) {
+        config(seqNum, "custom:session_desc", desc);
     }
 
-    void Control::configids(std::uint32_t seqNum, ClientSocket& s) {
+    void Control::configids(std::uint32_t seqNum) {
         std::string data = "AT*CONFIG_IDS=" + std::to_string(seqNum) + ",\"" + m_session_id + "\",\"" + "00000000" +
                            "\",\"" + m_app_id + "\"\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
-    void Control::config(std::uint32_t seqNum, std::string const& key, std::string const& value, ClientSocket& s) {
+    void Control::config(std::uint32_t seqNum, std::string const& key, std::string const& value) {
         std::string data = "AT*CONFIG=" + std::to_string(seqNum) + ",\"" + key + "\",\"" + value + "\"\r";
         M_traceFrame(data);
-        s.write(data);
+        Control::write(data);
     }
 
     void Control::M_trace(std::string const& msg) {
@@ -277,5 +277,10 @@ namespace lcontrol {
 #if PRINT_FRAMES
         std::cout << "[lcontrol::Control] " << msg << std::endl;
 #endif
+    }
+
+    void Control::write(std::string const &data) {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        m_sock->write(data);
     }
 }
